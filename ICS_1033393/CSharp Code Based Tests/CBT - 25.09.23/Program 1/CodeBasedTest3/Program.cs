6c307972016313dc0876ca15de3d15849a2b2f23
sanvatify <sanidhya.vatsa@gmail.com>
2023-09-28 12:00:17 +0530

    Updated

---
+++ ICS_1033393/CSharp Code Based Tests/CBT - 25.09.23/Program 1/CodeBasedTest3/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a program to find the Sum and the Average points scored by the teams in the IPL.
+//Create a Class called Cricket that has a function called Pointscalculation(int no_of_matches)
+//that takes no.of matches as input and accepts that many scores from the user.
+//The function should then display the Average and Sum of the scores.
+
+namespace CodeBasedTest3
+{
+    public class Cricket
+    {
+        
+        
+        static int totalRuns;
+        static double averageRuns;
+        static double totalPoints;
+
+        public static void Pointscalculation(int no_of_matches)
+        {
+            int[] MI = new int[no_of_matches];
+            int[] CSK = new int[no_of_matches];
+            int[] RCB = new int[no_of_matches];
+            int[] SRH = new int[no_of_matches];
+            Console.WriteLine("Select A Team To Enter Scores: 1. MI    2. CSK    3. RCB    4. SRH");
+            byte choice = Convert.ToByte(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    for (int i = 0; i < no_of_matches; i++)
+                    {
+                        Console.Write($"Match {i + 1}: ");
+                        MI[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine("Scores Updated");
+                    break;
+                case 2:
+                    for (int i = 0; i < no_of_matches; i++)
+                    {
+                        Console.Write($"Match {i + 1}: ");
+                        CSK[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine("Scores Updated");
+                    break;
+                case 3:
+                    for (int i = 0; i < no_of_matches; i++)
+                    {
+                        Console.Write($"Match {i + 1}: ");
+                        RCB[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine("Scores Updated");
+                    break;
+                case 4:
+                    for (int i = 0; i < no_of_matches; i++)
+                    {
+                        Console.Write($"Match {i + 1}: ");
+                        SRH[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine("Scores Updated");
+                    break;
+            }
+
+            Console.WriteLine("Data Available, Please Select A Team To View Stats");
+            Console.WriteLine("Select Team: 1. MI    2. CSK    3. RCB    4. SRH");
+            byte choiceNew = Convert.ToByte(Console.ReadLine());
+            switch (choiceNew)
+            {
+                case 1: for(int i =0; i<no_of_matches; i++)
+                    {
+                        totalRuns = MI.Sum();
+                        averageRuns = MI.Average();
+                        totalPoints = totalRuns * 0.2;
+                    }
+                    Console.WriteLine($"Total Runs: {totalRuns} \n" +
+                        $"Average Per Match: {averageRuns} \n" +
+                        $"Points Scored: {totalPoints}");
+                    break;
+                case 2:
+                    for (int i = 0; i < no_of_matches; i++)
+                    {
+                        totalRuns = CSK.Sum();
+                        averageRuns = CSK.Average();
+                        totalPoints = totalRuns * 0.2;
+                    }
+                    Console.WriteLine($"Total Runs: {totalRuns} \n" +
+                        $"Average Per Match: {averageRuns} \n" +
+                        $"Points Scored: {totalPoints}");
+                    break;
+                case 3:
+                    for (int i = 0; i < no_of_matches; i++)
+                    {
+                        totalRuns = RCB.Sum();
+                        averageRuns = RCB.Average();
+                        totalPoints = totalRuns * 0.2;
+                    }
+                    Console.WriteLine($"Total Runs: {totalRuns} \n" +
+                        $"Average Per Match: {averageRuns} \n" +
+                        $"Points Scored: {totalPoints}");
+                    break;
+                case 4:
+                    for (int i = 0; i < no_of_matches; i++)
+                    {
+                        totalRuns = SRH.Sum();
+                        averageRuns = SRH.Average();
+                        totalPoints = totalRuns * 0.2;
+                    }
+                    Console.WriteLine($"Total Runs: {totalRuns} \n" +
+                        $"Average Per Match: {averageRuns} \n" +
+                        $"Points Scored: {totalPoints}");
+                    break;
+            }
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** WELCOME ***");
+            Console.WriteLine("How Many IPL Matches?");
+            int numberOfMatches = int.Parse(Console.ReadLine());
+            Cricket.Pointscalculation(numberOfMatches);
+            Console.Read();
+        }
+    }
+}
---
+++ ICS_1033393/CSharp Code Based Tests/CBT - 25.09.23/Program 2/CodeBasedTestThree/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace CodeBasedTestThree
+{
+    public class Box
+    {
+        static double toBeAdded;
+        public double Len { get; set; }
+        public double Brd { get; set; }
+        public Box(double length, double breadth)
+        {
+            Len = length;
+        }
+        public Box(double breadth)
+        {
+            Brd = breadth;
+        }
+        public static Box AddBoxes(Box box1, Box box2)
+        {
+            toBeAdded = box1.Len + box2.Brd;
+
+            return new Box(toBeAdded);
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"\nWhen Added: {toBeAdded}");
+        }
+    }
+
+
+    class Testing
+    {
+        static void Main()
+        {
+            Console.Write("Length: ");
+            double l = double.Parse(Console.ReadLine());
+            Console.Write("Breadth: ");
+            double b = double.Parse(Console.ReadLine());
+            Box boxOne = new Box(l,b);
+            Box boxTwo = new Box(b);
+            Box box3 = Box.AddBoxes(boxOne, boxTwo);
+            box3.DisplayDetails();
+            Console.Read();
+        }
+    }
+}
